a794c0a3273cd1e7c1e043b192f89eac3e5d2b9b
Brian Hernandez <ricardo.hernandez@rightbalance.io>
2024-05-26 18:08:04 -0600

    cors enabled

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_graphql import GraphQLView
+from flask_cors import CORS
 from schema import schema
 
 app = Flask(__name__)
+CORS(app, resources={r"/graphql": {"origins": "*"}})  # Configuración específica de CORS
+
 app.add_url_rule(
     '/graphql',
     view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
 )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=3322)
